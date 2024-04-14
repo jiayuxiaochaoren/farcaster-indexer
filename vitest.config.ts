b1cfd0577da5152f3c9579d796a0874466c34686
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-04-14 01:04:34 -0400

    Add node about unnecessary package

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     testTimeout: 15_000,
+    // TODO: figure out how to use `Bun.env` instead of the `dotenv` package
     setupFiles: 'dotenv/config', // load variables form .env file
   },
 })
